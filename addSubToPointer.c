79f4852679c2d3327fdc7039518240af1645b5ef
mohbou <a.bourkaib@gmail.com>
2021-08-01 09:11:57 -0400

    commit

--- addSubToPointer.c
+++ addSubToPointer.c
@@ -9,5 +9,12 @@ int main(int argc, char const *argv[])
     long long c[]= {1,2,3};
     long long* p =c+1;
     printf("Result is %lld at address %p\n",*p,p);
+    long long result = &a - &b; //their difference in mem divided by sizeof(type of variable)
+    // multiply or divide pointer doesnt make sense
+    printf("Result is %lld with address %p and %p\n\n", result,&a,&b);
+    long long* ptr = malloc(sizeof(long long));
+    long long* q = malloc(sizeof(long long));
+    long long resultp = ptr -q;
+    printf("Result is %lld with address %p and %p\n\n", resultp,&ptr,&q);
     return 0;
 }
