361039b07ab2523462504d3864866206e8460c37
mohbou <a.bourkaib@gmail.com>
2021-08-01 13:01:21 -0400

    updating tuto

--- voidPointer.c
+++ voidPointer.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printBytes(int * arr, int n) {
+void printBytes(void * arr, int n) {
     int i;
     for ( i = 0; i < n; i++)
     {
-        printf("%08x ", arr[i]);// print out each element of the array x hexadecimal first 8 character
+        printf("%02x ", ((char*)arr)[i]);// print out each element of the array x hexadecimal first 8 character
         }
     }
     int main(int argc, char const *argv[])
     {
-        int arr[]= {16,31,32,65};
-    printBytes(arr,4);
+        long long arr[]= {16,31,32,65};
+    printBytes(arr,4 * sizeof(long long));
         return 0;
     }
         
